a7f5fa3dd77b332e991001cfe7e4e25b40c131ff
luannluna <luann.marques@gmail.com>
2025-07-27 23:30:32 -0300

    added DEMO

--- WillowKeyboard/SecureConfigurationManager.swift
+++ WillowKeyboard/SecureConfigurationManager.swift
@@ -1,10 +1,10 @@
 public class SecureConfigurationManager {
     public static let shared = SecureConfigurationManager()
     
-    private let deepgramApiKey = "54a8ca3bdf2c63e78138369d0e66275dedeb4c5e"
-    private let fleksyLicenseKey = "f001f040-796b-4461-b0ab-c602bbba909d"
-    private let fleksyLicenseSecret = "c1ced9261e612d3720f54839efc2d9cd"
-    
+    private let deepgramApiKey = "deepgramApiKey"
+    private let fleksyLicenseKey = "fleksyLicenseKey"
+    private let fleksyLicenseSecret = "fleksyLicenseSecret"
+
     private init() {}
     
     public func getDeepgramApiKey() -> String {
